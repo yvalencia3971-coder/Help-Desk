aa23927474eb5946308a5c9d1dcd87b4ea22d658
Yumberlly Valencia <yvalencia3971@utm.edu.ec>
2026-07-31 18:29:45 -0500

    Configuracion backend con PostgreSQL y Spring Boot funcionando

---
+++ Help-Desk-api/src/main/java/ec/edu/utm/Help_Desk_api/HelpDeskApiApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.utm.Help_Desk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskApiApplication.class, args);
+	}
+
+}
---
+++ Help-Desk-api/src/test/java/ec/edu/utm/Help_Desk_api/HelpDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.utm.Help_Desk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
